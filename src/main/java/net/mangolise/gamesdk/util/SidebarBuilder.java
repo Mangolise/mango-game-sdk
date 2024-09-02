7281fb104c9f936ad41c3f87b7476bb6d47bf8e5
Calcilore <github@calcilore.net>
2024-09-02 18:03:59 +1000

    Added SidebarBuilder

---
+++ src/main/java/net/mangolise/gamesdk/util/SidebarBuilder.java
@@ -0,0 +1,40 @@
+package net.mangolise.gamesdk.util;
+
+import net.kyori.adventure.text.Component;
+import net.minestom.server.scoreboard.Sidebar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SidebarBuilder {
+    private final List<Component> lines = new ArrayList<>();
+
+    public SidebarBuilder addLine(Component line) {
+        lines.add(line);
+        return this;
+    }
+
+    public SidebarBuilder addLine(String line) {
+        lines.add(Component.text(line));
+        return this;
+    }
+
+    public Sidebar build(Component title) {
+        Sidebar sidebar = new Sidebar(title);
+        apply(sidebar);
+
+        return sidebar;
+    }
+
+    public Sidebar build(String title) {
+        return build(Component.text(title));
+    }
+
+    public void apply(Sidebar sidebar) {
+        sidebar.getLines().forEach(line -> sidebar.removeLine(line.getId()));
+
+        for (int i = 0; i < lines.size(); i++) {
+            sidebar.createLine(new Sidebar.ScoreboardLine(String.valueOf(i), lines.get(i), lines.size() - i - 1));
+        }
+    }
+}
