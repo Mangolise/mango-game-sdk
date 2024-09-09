2a7c2490541fe9cc09ae7522a973044242285462
CoPokBl <copokbl@serble.net>
2024-09-09 12:13:00 +1000

    Use adventure legacy text converter instead of reimpl

--- src/main/java/net/mangolise/gamesdk/util/ChatUtil.java
+++ src/main/java/net/mangolise/gamesdk/util/ChatUtil.java
@@ -3,13 +3,11 @@ package net.mangolise.gamesdk.util;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.minestom.server.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class ChatUtil {
-    private static final char COLOR_CHAR = '&';
-    private static final char SPECIAL_CHAR = '§';
-    private static final String VALID_COLORS = "0123456789abcdefklmnor";
 
     /**
      * Converts a legacy & color code string to a modern Adventure Component.
@@ -18,58 +16,7 @@ public class ChatUtil {
      */
     public static Component toComponent(String message, Object... args) {
         message = String.format(message, args);
-
-        Component component = Component.empty();
-        StringBuilder currentSection = new StringBuilder();
-
-        NamedTextColor color = NamedTextColor.WHITE;
-        TextDecoration decoration = null;
-        for (int i = 0; i < message.length(); i++) {
-            if (message.charAt(i) != COLOR_CHAR || !VALID_COLORS.contains(String.valueOf(message.charAt(i + 1)))) {
-                currentSection.append(message.charAt(i));
-                continue;
-            }
-
-            // Apply prev section
-            if (!currentSection.isEmpty()) {
-                component = applyComponent(component, currentSection, color, decoration);
-                currentSection = new StringBuilder();
-            }
-
-            i++;
-
-            char modifier = message.charAt(i);
-            decoration = null;
-            switch (modifier) {
-                case '0' -> color = NamedTextColor.BLACK;
-                case '1' -> color = NamedTextColor.DARK_BLUE;
-                case '2' -> color = NamedTextColor.DARK_GREEN;
-                case '3' -> color = NamedTextColor.DARK_AQUA;
-                case '4' -> color = NamedTextColor.DARK_RED;
-                case '5' -> color = NamedTextColor.DARK_PURPLE;
-                case '6' -> color = NamedTextColor.GOLD;
-                case '7' -> color = NamedTextColor.GRAY;
-                case '8' -> color = NamedTextColor.DARK_GRAY;
-                case '9' -> color = NamedTextColor.BLUE;
-                case 'a' -> color = NamedTextColor.GREEN;
-                case 'b' -> color = NamedTextColor.AQUA;
-                case 'c' -> color = NamedTextColor.RED;
-                case 'd' -> color = NamedTextColor.LIGHT_PURPLE;
-                case 'e' -> color = NamedTextColor.YELLOW;
-                case 'f', 'r' -> color = NamedTextColor.WHITE;
-                case 'k' -> decoration = TextDecoration.OBFUSCATED;
-                case 'l' -> decoration = TextDecoration.BOLD;
-                case 'm' -> decoration = TextDecoration.STRIKETHROUGH;
-                case 'n' -> decoration = TextDecoration.UNDERLINED;
-                case 'o' -> decoration = TextDecoration.ITALIC;
-            }
-        }
-
-        if (!currentSection.isEmpty()) {
-            component = applyComponent(component, currentSection, color, decoration);
-        }
-
-        return component;
+        return LegacyComponentSerializer.legacy('&').deserialize(message);
     }
 
     public static @Nullable NamedTextColor codeToNamedTextColor(String code) {
